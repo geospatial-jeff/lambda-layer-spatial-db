605e9f19d9249e4beb12d8e62c604381646386d1
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-04-10 15:56:35 -0600

    wraping with click cli

--- _cli.py
+++ _cli.py
@@ -2,6 +2,7 @@ import json
 
 import click
 from db import Database
+from analyze import choose_res
 
 
 @click.group()
@@ -11,9 +12,16 @@ def lambda_db():
 
 @lambda_db.command(name="build")
 @click.argument('db_path')
-@click.argument('geojson', type=click.File('r'))
+@click.argument('feature_collection', type=click.File('r'))
 def build(db_path, feature_collection):
     data = json.load(feature_collection)
 
     with Database.load(db_path) as db:
         db.load_features(data)
+
+@lambda_db.command(name="analyze")
+@click.argument('feature_collection', type=click.File('r'))
+@click.option('--optimize', '-o', default='size', type=str)
+def analyze(feature_collection, optimize):
+    data = json.load(feature_collection)
+    choose_res(data, optimize)
