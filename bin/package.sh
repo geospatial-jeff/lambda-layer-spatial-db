0a186ff929c6b5c035de50ddafb1e71262ab9d13
Jeff Albrecht <geospatialjeff@gmail.com>
2019-04-19 11:51:55 -0600

    excluding shapely

--- bin/package.sh
+++ bin/package.sh
@@ -16,7 +16,7 @@ strip $DEPLOY_DIR/lib/* || true
 
 # Moving python libraries
 mkdir $DEPLOY_DIR/python
-EXCLUDE="urllib3* s3transfer* boto3* botocore* pip* docutils* *.pyc setuptools* wheel* coverage* testfixtures* mock* *.egg-info *.dist-info __pycache__ easy_install.py"
+EXCLUDE="shapely* urllib3* s3transfer* boto3* botocore* pip* docutils* *.pyc setuptools* wheel* coverage* testfixtures* mock* *.egg-info *.dist-info __pycache__ easy_install.py"
 
 EXCLUDES=()
 for E in ${EXCLUDE}
